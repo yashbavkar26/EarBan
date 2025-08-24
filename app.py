a2e2ce6d5b8af29db5c30217dbcfb9dacd939f3e
Yash Bavkar <yashbavkar26@gmail.com>
2025-08-24 17:22:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,8 +74,8 @@ background-attachment: fixed;
 /* Logo positioning */
 .logo-container {{
     position: fixed;  /* stays fixed when scrolling */
-    top: 200px;        /* adjust distance from top */
-    right: 600px;      /* adjust distance from right */
+    top: 100px;        /* adjust distance from top */
+    right: 400px;      /* adjust distance from right */
     z-index: 9999;    /* ensures it's always on top */
 }}
 .logo-container img {{
